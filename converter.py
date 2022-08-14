cf08d5fd25e9bbb0134b92b3884480fb5d18d8b5
Monaheng <ntaimonaheng@gmail.com>
2022-08-14 20:56:33 +0200

    Comments cleanup

--- converter.py
+++ converter.py
@@ -1,21 +1,6 @@
 from locale import currency
 from forex_python.converter import CurrencyRates
 
-#  print("test")
-
-#  cr = CurrencyRates()
-
-#  amount = int(input("Please enter the amount you want to convert: "))
-
-#  from_currency = input("Please enter the currency code that has to be converted: ").upper()
-
-#  to_currency = input("Please enter the currency code to convert: ").upper()
-
-#  print("You are converting", amount, from_currency, "to", to_currency,".")
-
-#  output = cr.convert(from_currency, to_currency, amount)
-
-#  print("The converted rate is:", output)
 
 def menu():
     print("\n\t<<MENU>>\n")
@@ -66,22 +51,6 @@ def convert(list1):
         output = None
 
     return output
-
-
-#  amount = int(input("Please enter the amount you want to convert: "))
-
-#  from_currency = input("Please enter the currency code that has to be converted: ").upper()
-
-#  to_currency = input("Please enter the currency code to convert: ").upper()
-
-#  print("You are converting", amount, from_currency, "to", to_currency,".")
-
-#  output = cr.convert(from_currency, to_currency, amount)
-
-#  print("The converted rate is:", output)
-
-
-
 
 
 if __name__ == '__main__':
